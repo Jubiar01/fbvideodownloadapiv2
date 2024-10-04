d4b83c426e3b7b554fb719516ce79b4b737b9c0a
secret <146640389+ryoheixycz@users.noreply.github.com>
2024-10-04 18:51:11 +0800

    Update api.js

--- api.js
+++ api.js
@@ -12,7 +12,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
 // Welcome endpoint
-app.get('/welcome', (req, res) => {
+app.get('/', (req, res) => {
     res.json({ message: "Welcome to SaveFace API!" });
 });
 
